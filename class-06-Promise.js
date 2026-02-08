ca1da72bfdf9bcb4405ebf8fd8c4fbe8f9c5d0c6
Asif Hussain <158553805+asifmeer55766@users.noreply.github.com>
2026-02-08 11:08:34 +0500

    add new promses question

--- class-06-Promise.js
+++ class-06-Promise.js
@@ -1,25 +1,85 @@
-// simple example
-
-// console.log("implementation example of promise");
-// const promise = new Promise((resolve, reject) => {
-
-//   if (resolve) {
-//     console.log("promise resolve true : success");
-//   } else {
-//     console.log("promise reject");
-//   }
-// });
-
+// example for promise in javascript
 const API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=10";
-
-const fetchData = new Promise((resolve, reject) => {
+// fetch api data using promies
+/*const fetchData = new Promise((resolve, reject) => {
   fetch(API_URL)
     .then((response) => response.json())
     .then((data) => resolve(data))
     .catch((error) => reject(error));
 });
-console.log("fetchdata : ", fetchData);
+fetchData.then((data) => console.log(data)).catch(console.log("error"));
+*/
+// ========================================================================
+// fetch same api data using async await
+/*const fetchApi = async () => {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    console.log("api data using async and await ", data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-fetchData
+fetchApi();*/
+// ========================================================================
+// question no 3
+/*const fetchUserData = (userId) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!userId || userId <= 0) {
+        reject("invalid user Id");
+      } else {
+        resolve({
+          id: userId,
+          name: "Asif Hussain",
+          role: "Developer",
+        });
+      }
+    }, 2000);
+  });
+};
+fetchUserData(-6)
+  .then((data) => {
+    console.log("user name : ", data.name);
+    console.log("user name : ", data.role);
+  })
+  .catch((error) => {
+    console.log("error", error);
+  })
+  .finally(() => {
+    console.log("request completed");
+  });*/
+// ========================================================================
+
+/* question no 4 ,Problem Statement: Order Payment Processing Using Promises. 
+You need to simulate an online order payment process using JavaScript Promises.*/
+
+const processPayment = (order) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (
+        !order ||
+        typeof order.orderId !== "number" ||
+        order.amount <= 0 ||
+        (order.paymentMethod !== "card" && order.paymentMethod !== "upi")
+      ) {
+        rej("Payment failed");
+      } else {
+        res({
+          orderId: order.orderId,
+          status: "Success",
+          message: "Payment processed successfully",
+        });
+      }
+    }, 1500);
+  });
+};
+processPayment({
+  orderId: 101,
+  amount: 10,
+  paymentMethod: "upi",
+})
   .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+  .catch((error) => console.log(error, "Error"))
+  .finally(() => console.log("Payment request completed"));
