d4039964dbef0d17335a936d4d20b9733f253d49
Asif Hussain <158553805+asifmeer55766@users.noreply.github.com>
2026-02-16 11:13:23 +0500

    add some interview question

--- class-07-this.js
+++ class-07-this.js
@@ -1,11 +1,32 @@
-const name = "ali";
-// in browser name refers to window.name = ali
-// but in Node js this refer to {} so it will be undefind
-const obj = {
-  name: "Ahmad",
-  printName: () => {
-    console.log("name is:", this.name);
-  },
-};
+// const name = "ali";
+// // in browser name refers to window.name = ali
+// // but in Node js this refer to {} so it will be undefind
+// const obj = {
+//   name: "Ahmad",
+//   printName: () => {
+//     console.log("name is:", this.name);
+//   },
+// };
 
-console.log(obj.printName());
+// console.log(obj.printName());
+
+// console.log([] == ![]);
+// let a = 10;
+
+// function test() {
+//   console.log(a);
+//   let a = 20;
+// }
+
+// test();
+
+// {
+//   let a = 10;
+//   var b = 20;
+// }
+
+// console.log(b);
+// console.log(a);
+for (var i = 1; i < 3; i++) {
+  setTimeout(() => console.log(i), 0);
+}
